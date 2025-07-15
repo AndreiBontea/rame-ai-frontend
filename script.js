b3a1a9cf0544d32e72c1b379a24b170044e8eef9
AndreiBontea <fvndbsh@gmail.com>
2025-07-15 18:58:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -105,6 +105,7 @@ async function trimitePrompt() {
 
   try {
  const response = await fetch("https://HauntingSpeedyIrcbot--contact2207.repl.co/api/recomanda", {
+
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
